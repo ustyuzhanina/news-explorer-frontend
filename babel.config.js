87bef092cea308956467eb737bac1bc8ce6d33f2
ustyuzhanina <elena.ustyuzhanina.rus@gmail.com>
2020-10-15 00:04:49 +0700

    ready webpack and git config

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+const presets = [
+  [
+    '@babel/env',
+    {
+      targets: { // указать цели, для полифилов
+        esmodules: true,
+        ios: '12',
+        edge: '17',
+        firefox: '60',
+        chrome: '64',
+        android: '67',
+        safari: '11.1',
+      },
+      useBuiltIns: 'usage', // эта настройка babel-polyfill, если стоит значение usage, то будут подставлятся полифилы для версий браузеров которые указали ниже.
+      corejs: '3.1.4', // явно проставить версию corejs
+    },
+  ],
+];
+
+module.exports = { presets };
