f3a6ab12223fd79070c3d86189b396ad09655757
ustyuzhanina <elena.ustyuzhanina.rus@gmail.com>
2021-01-15 03:34:05 +0700

    enabled show-menu-btn switching from hamburger to cross and back on click

--- src/script/script.js
+++ src/script/script.js
@@ -26,14 +26,14 @@ export default (function () {
 
   popupEnterButton.addEventListener('click', () => {
     navbar.classList.remove('navbar_opened');
-    navbarBtn.style.display = 'none';
+    // navbarBtn.style.display = 'none';
     popupEnter.classList.toggle('popup_is-opened');
   });
 
   function closePopup() {
     const openPopup = event.target.closest('.popup');
     openPopup.classList.toggle('popup_is-opened');
-    navbarBtn.style.display = 'block';
+    // navbarBtn.style.display = 'block';
   }
 
   popupEnterCloseButton.addEventListener('click', closePopup);
@@ -50,14 +50,14 @@ export default (function () {
     function toggleSuccessPopup() {
       popupSuccessReg.classList.toggle('popup_is-opened');
     }
-    function showNavbarBtn() {
-      navbarBtn.style.display = 'block';
-    }
+    // function showNavbarBtn() {
+    //   navbarBtn.style.display = 'block';
+    // }
     popupRegister.classList.toggle('popup_is-opened');
-    navbarBtn.style.display = 'none';
+    // navbarBtn.style.display = 'none';
     toggleSuccessPopup();
     setTimeout(toggleSuccessPopup, 3000);
-    setTimeout(showNavbarBtn, 3001);
+    // setTimeout(showNavbarBtn, 3001);
   });
 
   popupEnterGoToRegister.addEventListener('click', () => {
